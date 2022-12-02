ccb5bc1c6e780a44520390dacb314d0e3e3669ac
Shane Lx <39976362+shanefully-done@users.noreply.github.com>
2022-12-02 21:27:04 +0900

    Fixed copy path

--- copy.sh
+++ copy.sh
@@ -2,17 +2,17 @@
 
 if [[ -z $1 ]]
 then
-	cp ~/.cache/wal/colors-mailspring-index.less ~/.config/Mailspring/packages/Pywal/styles/index.less
-	cp ~/.cache/wal/colors-mailspring-theme-colors.less ~/.config/Mailspring/packages/Pywal/styles/theme-colors.less
-	cp ~/.cache/wal/colors-mailspring-ui-variables.less ~/.config/Mailspring/packages/Pywal/styles/ui-variables.less
+	cp ~/.cache/wal/colors-mailspring-index.less ~/.config/Mailspring/packages/Pywal-Mailspring/styles/index.less
+	cp ~/.cache/wal/colors-mailspring-theme-colors.less ~/.config/Mailspring/packages/Pywal-Mailspring/styles/theme-colors.less
+	cp ~/.cache/wal/colors-mailspring-ui-variables.less ~/.config/Mailspring/packages/Pywal-Mailspring/styles/ui-variables.less
 elif [ $1 = "macos" ]
 then
-	cp ~/.cache/wal/colors-mailspring-index.less "~/Library/Application Support/Mailspring/packages/Pywal/styles/index.less"
-	cp ~/.cache/wal/colors-mailspring-theme-colors.less "~/Library/Application Support/Mailspring/packages/Pywal/styles/theme-colors.less"
-	cp ~/.cache/wal/colors-mailspring-ui-variables.less "~/Library/Application Support/Mailspring/packages/Pywal/styles/ui-variables.less"
+	cp ~/.cache/wal/colors-mailspring-index.less "~/Library/Application Support/Mailspring/packages/Pywal-Mailspring/styles/index.less"
+	cp ~/.cache/wal/colors-mailspring-theme-colors.less "~/Library/Application Support/Mailspring/packages/Pywal-Mailspring/styles/theme-colors.less"
+	cp ~/.cache/wal/colors-mailspring-ui-variables.less "~/Library/Application Support/Mailspring/packages/Pywal-Mailspring/styles/ui-variables.less"
 elif [ $1 = "snap" ]
 then
-	cp ~/.cache/wal/colors-mailspring-index.less ~/snap/mailspring/common/packages/Pywal/styles/index.less
-	cp ~/.cache/wal/colors-mailspring-theme-colors.less ~/snap/mailspring/common/packages/Pywal/styles/theme-colors.less
-	cp ~/.cache/wal/colors-mailspring-ui-variables.less ~/snap/mailspring/common/packages/Pywal/styles/ui-variables.less
+	cp ~/.cache/wal/colors-mailspring-index.less ~/snap/mailspring/common/packages/Pywal-Mailspring/styles/index.less
+	cp ~/.cache/wal/colors-mailspring-theme-colors.less ~/snap/mailspring/common/packages/Pywal-Mailspring/styles/theme-colors.less
+	cp ~/.cache/wal/colors-mailspring-ui-variables.less ~/snap/mailspring/common/packages/Pywal-Mailspring/styles/ui-variables.less
 fi
